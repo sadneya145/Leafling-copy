fec33386a1782a1c1ffb44900225e59a0c41191e
sadneya145 <144504571+sadneya145@users.noreply.github.com>
2024-10-19 11:17:38 +0530

    Update CreatePost.js

--- frontend/src/Components/SocilaMedia/Post/CReatePost/CreatePost.js
+++ frontend/src/Components/SocilaMedia/Post/CReatePost/CreatePost.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import './CreatePost.css'; // Import the CSS file
 
 const CreatePost = () => {
